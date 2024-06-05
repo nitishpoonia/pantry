600a34c7a1d01fa8674de8a66ed997645905ed30
Nitish Poonia <npoonia39@gmail.com>
2024-06-05 08:18:23 +0530

    Sign up created and made responsive

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,114 +1,66 @@
 import React, { useState } from "react";
-import "./Signup.css";
-const Signup = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import "./Login.css";
 
-  // States for checking the errors
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState(false);
+import coffeeCup from "../assets/login-cup.jpeg";
 
-  // Handling the name change
-  const handleName = (e) => {
-    setName(e.target.value);
-    setSubmitted(false);
-  };
-
-  // Handling the email change
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    setSubmitted(false);
-  };
-
-  // Handling the password change
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-    setSubmitted(false);
-  };
-
-  // Handling the form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name === "" || email === "" || password === "") {
-      setError(true);
-    } else {
-      setSubmitted(true);
-      setError(false);
-    }
-  };
-
-  // Showing success message
-  const successMessage = () => {
-    return (
-      <div
-        className="success"
-        style={{
-          display: submitted ? "" : "none",
-        }}
-      >
-        <h1>User {name} successfully registered!!</h1>
-      </div>
-    );
-  };
-
-  // Showing error message if error is true
-  const errorMessage = () => {
-    return (
-      <div
-        className="error"
-        style={{
-          display: error ? "" : "none",
-        }}
-      >
-        <h1>Please enter all the fields</h1>
-      </div>
-    );
-  };
+const Login = () => {
   return (
-    <div className="form">
-      <div>
-        <h1>User Registration</h1>
+    <div className="outer-container">
+      <div className="container">
+        <div className="left-container">
+          <img src={coffeeCup} alt="Cup of Coffee" />
+        </div>
+        <div className="right-outer-container">
+          <div className="right-inner-container">
+            <div className="top-details">
+              <p className="heading">Welcome</p>
+              <p className="desc">
+                Enter your details to create your account and start your journey
+              </p>
+            </div>
+
+            <form className="form">
+              <div className="details-container">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="form-input"
+                />
+              </div>
+              <div className="details-container">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  className="form-input"
+                />
+              </div>
+              <div className="details-container">
+                <label htmlFor="password">Re-enter Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  className="form-input"
+                />
+              </div>
+
+              <button type="submit" className="btn login-btn">
+                Create Account
+              </button>
+            </form>
+            <p>Already have an account? Sign in</p>
+          </div>
+        </div>
       </div>
-
-      {/* Calling to the methods */}
-      <div className="messages">
-        {errorMessage()}
-        {successMessage()}
-      </div>
-
-      <form>
-        {/* Labels and inputs for form data */}
-        <label className="label">Name</label>
-        <input
-          onChange={handleName}
-          className="input"
-          value={name}
-          type="text"
-        />
-
-        <label className="label">Email</label>
-        <input
-          onChange={handleEmail}
-          className="input"
-          value={email}
-          type="email"
-        />
-
-        <label className="label">Password</label>
-        <input
-          onChange={handlePassword}
-          className="input"
-          value={password}
-          type="password"
-        />
-
-        <button onClick={handleSubmit} className="btn" type="submit">
-          Submit
-        </button>
-      </form>
     </div>
   );
 };
 
-export default Signup;
+export default Login;
